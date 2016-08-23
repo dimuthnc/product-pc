255e0e9f7db2c2cde6ebe0c41ea54a06b2062b14
Dimuth <dimuthu.13@cse.mrt.ac.lk>
2016-08-23 15:16:00 +0530

    BPMNInstance Objects can be received by PC now

--- modules/components/core/org.wso2.carbon.pc.core/src/main/java/org/wso2/carbon/pc/core/InstanceService.java
+++ modules/components/core/org.wso2.carbon.pc.core/src/main/java/org/wso2/carbon/pc/core/InstanceService.java
@@ -1,7 +1,15 @@
 package org.wso2.carbon.pc.core;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.bpmn.core.mgt.model.xsd.BPMNInstance;
 import org.wso2.carbon.pc.core.assets.Instance;
+import org.wso2.carbon.pc.core.config.ProcessCenterConfiguration;
+import org.wso2.carbon.pc.core.runtime.ProcessServer;
+import org.wso2.carbon.pc.core.runtime.ProcessServerImpl;
+import org.wso2.carbon.utils.CarbonUtils;
+
+import java.io.File;
 
 /**
  * Created by dimuth on 8/23/16.
@@ -9,10 +17,21 @@ import org.wso2.carbon.pc.core.assets.Instance;
 
 public class InstanceService {
     Instance ins;
-    public InstanceService (String username, String password, String url){
+    private ProcessCenterConfiguration processCenterConfiguration;
+    private ProcessServer processServer;
+    private static final Log log = LogFactory.getLog(ProcessCenter.class);
+    public InstanceService (){
+        loadProcessCenterConfiguration();
+        try {
+            loadRuntimeEnvironment();
+        } catch (ProcessCenterException e) {
+            e.printStackTrace();
+        }
+
 
         try {
-            ins = new Instance(username,password,url);
+            ins = new Instance(processCenterConfiguration.getRuntimeEnvironmentUsername(), processCenterConfiguration
+                    .getRuntimeEnvironmentPassword(),processCenterConfiguration.getRuntimeEnvironmentURL());
         } catch (ProcessCenterException e) {
             e.printStackTrace();
         }
@@ -23,8 +42,9 @@ public class InstanceService {
         BPMNInstance[] list ;
         try {
 
+
             list =ins.getInstanceList();
-            System.out.println(list);
+            
             return list;
 
         } catch (ProcessCenterException e) {
@@ -33,5 +53,47 @@ public class InstanceService {
         return null;
 
     }
+    private void loadProcessCenterConfiguration() {
+        if (log.isDebugEnabled()) {
+            log.debug("Loading Process Center Configuration.");
+        }
+        if (isProcessCenterConfigurationFileAvailable()) {
+            File processCenterConfigFile = new File(getProcessCenterConfigurationFilePath());
+            processCenterConfiguration = new ProcessCenterConfiguration(processCenterConfigFile);
+        } else {
+            log.warn("Process Center configuration file: " + ProcessCenterConstants
+                    .PROCESS_CENTER_CONFIGURATION_FILE_NAME +
+                    " not found. Loading default configurations.");
+        }
+    }
+
+    /**
+     * Load Process Runtime Servers
+     */
+    private void loadRuntimeEnvironment() throws ProcessCenterException {
+        if (log.isDebugEnabled()) {
+            log.debug("Loading Process Runtime Servers.");
+        }
+        // load the runtime server configs from configuration
+        if (processCenterConfiguration != null && processCenterConfiguration.isRuntimeEnvironmentEnabled()) {
+            processServer = new ProcessServerImpl(processCenterConfiguration.getRuntimeEnvironmentURL(),
+                    processCenterConfiguration.getRuntimeEnvironmentUsername(), processCenterConfiguration
+                    .getRuntimeEnvironmentPassword());
+        }
+        // TODO: support multiple runtime enviroments
+    }
+    private boolean isProcessCenterConfigurationFileAvailable() {
+        File processCenterConfigurationFile = new File(getProcessCenterConfigurationFilePath());
+        return processCenterConfigurationFile.exists();
+    }
+
+    /**
+     * @return Process Center configuration path.
+     */
+    private String getProcessCenterConfigurationFilePath() {
+        return CarbonUtils.getCarbonConfigDirPath() + File.separator +
+                ProcessCenterConstants.PROCESS_CENTER_CONFIGURATION_FILE_NAME;
+    }
 
 }
+
--- modules/components/core/org.wso2.carbon.pc.core/src/main/java/org/wso2/carbon/pc/core/assets/Instance.java
+++ modules/components/core/org.wso2.carbon.pc.core/src/main/java/org/wso2/carbon/pc/core/assets/Instance.java
@@ -1,48 +1,72 @@
 package org.wso2.carbon.pc.core.assets;
 
 import org.apache.axis2.AxisFault;
+import org.apache.axis2.jaxws.util.SoapUtils;
+import org.apache.taglibs.standard.tag.el.sql.SetDataSourceTag;
 import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
 import org.wso2.carbon.bpmn.core.mgt.model.xsd.BPMNInstance;
 import org.wso2.carbon.bpmn.stub.BPMNDeploymentServiceBPSFaultException;
 import org.wso2.carbon.bpmn.stub.BPMNInstanceServiceBPSFaultException;
+import org.wso2.carbon.pc.core.ProcessCenter;
+import org.wso2.carbon.pc.core.ProcessCenterConstants;
 import org.wso2.carbon.pc.core.ProcessCenterException;
 import org.wso2.carbon.pc.core.clients.LoginAdminServiceClient;
 import org.wso2.carbon.pc.core.clients.WorkflowServiceClient;
+import org.wso2.carbon.pc.core.config.ProcessCenterConfiguration;
+import org.wso2.carbon.pc.core.runtime.ProcessServer;
+import org.wso2.carbon.pc.core.runtime.ProcessServerImpl;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.utils.CarbonUtils;
 
 /**
  * Created by dimuth on 8/23/16.
  */
 public class Instance {
+    BPMNInstance[] instances;
+    BPMNInstance inst =new BPMNInstance();
     LoginAdminServiceClient loginServiceClient;
-    String username ;
-    String password ;
-    String url ;
+
+    String username;
+    String password;
+    String url;
 
 
-    public Instance(String usr, String psw, String url) throws ProcessCenterException{
+    public Instance(String usr, String psw, String url) throws ProcessCenterException {
+
         this.url = url;
-        this.username=usr;
-        this.password= psw;
-        System.out.println("Creating instance account");
+        this.username = usr;
+        this.password = psw;
+        System.out.println("Creating instance object");
+        System.out.println(url);
         try {
+
             loginServiceClient = new LoginAdminServiceClient(this.url);
         } catch (AxisFault axisFault) {
             throw new ProcessCenterException("Error while creating Login admin service client ", axisFault);
         }
+        System.out.println("object Created");
+
 
     }
 
 
-
     public BPMNInstance[] getInstanceList() throws ProcessCenterException {
+
         try {
+            System.out.println("Inside the method try");
             WorkflowServiceClient workflowServiceClient = new WorkflowServiceClient(loginServiceClient.authenticate
                     (this.username, this.password.toCharArray()), this.url, null);
 
-            return workflowServiceClient.getInstanceList();
+
+            instances = workflowServiceClient.getInstanceList();
+            System.out.println(instances);
+            return instances;
 
         } catch (LoginAuthenticationExceptionException e) {
             throw new ProcessCenterException("Authentication error while undeploying bpmn package to BPS server ", e);
@@ -54,6 +78,7 @@ public class Instance {
             e.printStackTrace();
 
         }
+
         return null;
     }
 }
